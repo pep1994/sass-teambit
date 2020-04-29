c59592fecce9e69bb34bc19c809f3f929f0f75ef
pep1994 <marcopetrini1994@live.it>
2020-04-29 16:06:34 +0200

    section hero - stile

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').copy('src/index.html', 'dist/');
+mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').copy('src/index.html', 'dist/').options({
+  processCssUrls: false
+});
 
