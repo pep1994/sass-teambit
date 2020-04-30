4bc54e9488eb37ac872dda82d3860480c91516c7
pep1994 <marcopetrini1994@live.it>
2020-04-30 17:04:24 +0200

    effetto scroll header e gestione hover con js

--- dist/app.js
+++ dist/app.js
@@ -95,6 +95,8 @@
 
 $(document).ready(function () {
   var itemMenu = $('.menu li');
+  var scrolldocument = $(document).scrollTop();
+  console.log(scrolldocument);
   itemMenu.mouseenter(function () {
     $(this).find('.menu-tendina').fadeIn('fast');
   });
@@ -105,6 +107,49 @@ $(document).ready(function () {
     $('.hamburger-bars').toggle();
     $('.close').toggle();
     $('.hamburger-menu').toggleClass('active');
+  });
+  $(document).scroll(function () {
+    var currentScroll = $(document).scrollTop();
+
+    if (scrolldocument > currentScroll) {
+      $('header').css('top', 0);
+    } else {
+      $('header').css('top', '-100px');
+    }
+
+    scrolldocument = currentScroll;
+
+    if (!scrolldocument == 0) {
+      $('header nav .menu li.sign-up ').css({
+        'background-color': '#2F478F'
+      });
+      $('header nav .menu li.sign-up a ').css({
+        'color': '#fff'
+      });
+    } else {
+      $('header nav .menu li.sign-up ').css({
+        'background-color': '#fff'
+      });
+      $('header nav .menu li.sign-up a ').css({
+        'color': '#2F478F'
+      });
+    }
+  });
+  $('header nav .menu li.sign-up').mouseenter(function () {
+    $(this).css({
+      'background-color': '#2F478F'
+    });
+    $(this).find('a').css({
+      'color': '#fff'
+    });
+  });
+  $('header nav .menu li.sign-up').mouseleave(function () {
+    $(this).css({
+      'background-color': '#fff'
+    });
+    $(this).find('a').css({
+      'color': '#2F478F'
+    });
   });
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
+  
 
   var itemMenu = $('.menu li');
-  
+ 
+  var scrolldocument = $(document).scrollTop();
+  console.log(scrolldocument);
+
+
   itemMenu.mouseenter(function () { 
   
     $(this).find('.menu-tendina').fadeIn('fast');
@@ -22,4 +27,58 @@ $(document).ready(function () {
 
   });
 
+  $(document).scroll(function () { 
+    var currentScroll = $(document).scrollTop();
+
+    if (scrolldocument > currentScroll) {
+
+      $('header').css('top', 0);
+
+    } else {
+      
+      $('header').css('top', '-100px');
+    }
+
+    scrolldocument = currentScroll;
+
+    if (!scrolldocument == 0) {
+      $('header nav .menu li.sign-up ').css({
+        'background-color': '#2F478F'
+      });
+      $('header nav .menu li.sign-up a ').css({
+        'color': '#fff'
+      });
+
+    } else {
+      $('header nav .menu li.sign-up ').css({
+        'background-color': '#fff'
+      });
+      $('header nav .menu li.sign-up a ').css({
+        'color': '#2F478F'
+      });
+    }
+    
+  });
+
+  $('header nav .menu li.sign-up').mouseenter(function () { 
+    $(this).css({
+      'background-color': '#2F478F'
+    });
+    
+    $(this).find('a').css({
+      'color': '#fff'
+    });
+
+  });
+
+  $('header nav .menu li.sign-up').mouseleave(function () {
+    $(this).css({
+      'background-color': '#fff'
+    });
+
+    $(this).find('a').css({
+      'color': '#2F478F'
+    });
+
+  });
 });
